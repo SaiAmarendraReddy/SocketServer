f7e61ad353924885d5ebfc18f3950a12f7a9cce6
SaiAmarendraReddy <saiamarendrareddy@gmail.com>
2021-06-13 09:44:42 +0530

    inital serversocket

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server,{
+  cors:{
+    origin:"*",
+  }
+});
+const port = 2255 || process.env.PORT;
+
+io.on('connection',(client)=>{
+  console.log("connection establish");
+  /* console.log(client) */
+  client.on("fromclient",(data)=>{
+    
+    client.broadcast.emit('toclient',{msg:data['fromclient'],InOut: "incoming common"});
+  })
+})
+
+server.listen(port,()=>{
+  console.log(`listing on port ${port}`)
+})
