88d1ad85a815a7b1ec987d32c1ade3e50ebbc1bb
SaiAmarendraReddy <saiamarendrareddy@gmail.com>
2021-06-17 16:22:49 +0530

    added disconnected

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const io = require("socket.io")(server,{
   }
 });
 const port = 2255 ;
+const onlineUser = [];
 
 io.on('connection',(client)=>{
-  console.log("connection establish");
+  console.log("connection establish",client.id);
+
+  /* online user */
+  client.on('onlineActive',(online)=>{
+    /* push the user to the array */
+    onlineUser.push(online);
+    /* send online user list to the client */
+    client.broadcast.emit('activeUser',onlineUser);
+
+    console.log(client.id,online);
+  })
+
   /* console.log(client) */
   client.on("fromclient",(data)=>{
     
     client.broadcast.emit('toclient',{msg:data['fromclient'],InOut: "incoming common"});
   })
+
+  /* disconnect*/
+  client.on("disconnect", (reason) => {
+    console.log(client.id+"dsic")
+  });
 })
 
 server.listen(port,()=>{
